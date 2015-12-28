ee631aa8b11e0ecb270a063e32ce8e9dd7799db3
sgomez <santii810@hotmail.com>
2015-12-28 22:23:16 +0100

    Insertado fragment de google maps

--- app/src/main/java/sgomez/ejercicios/apuntaextra/Activities/AddLocalActivity.java
+++ app/src/main/java/sgomez/ejercicios/apuntaextra/Activities/AddLocalActivity.java
@@ -1,37 +1,22 @@
 package sgomez.ejercicios.apuntaextra.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import android.view.View;
 
 import sgomez.ejercicios.apuntaextra.R;
 
 
-public class AddLocalActivity extends FragmentActivity implements OnMapReadyCallback {
+public class AddLocalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_local);
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
     }
 
-    @Override
-    public void onMapReady(GoogleMap map) {
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    public void buttonMapAdressSelectOnClick(View view) {
+        startActivity(new Intent(this, addMapPositionActivity.class));
     }
 }
