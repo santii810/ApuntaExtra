9dfb5e49a40119488aac27d69e8939f6d707d639
sgomez <santii810@hotmail.com>
2015-12-30 16:10:04 +0100

    Actualizado archivo gitignore y pequeños ajustes de diseño

--- app/src/main/java/sgomez/ejercicios/apuntaextra/Activities/MainActivity.java
+++ app/src/main/java/sgomez/ejercicios/apuntaextra/Activities/MainActivity.java
@@ -30,7 +30,9 @@ public class MainActivity extends AppCompatActivity implements
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 9001;
     private static GoogleApiClient mGoogleApiClient;
+
     private static Usuario usuario;
+
     //REPOSITORIOS
     private static ParseUsuarioRepository UsuarioRepository = new ParseUsuarioRepository();
     private static ParseLocalRepository LocalRepository = new ParseLocalRepository();
