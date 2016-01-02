37061d1399ea33f6866a2c2ca27dd8cdfb94878b
sgomez <santii810@hotmail.com>
2016-01-02 01:16:41 +0100

    Insertada action bar en addLocal para guardar los datos

--- app/src/main/java/sgomez/ejercicios/apuntaextra/Activities/AddLocalActivity.java
+++ app/src/main/java/sgomez/ejercicios/apuntaextra/Activities/AddLocalActivity.java
@@ -3,6 +3,8 @@ package sgomez.ejercicios.apuntaextra.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -68,5 +70,35 @@ public class AddLocalActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_add_local, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addLocalMenuSave:
+                if (((EditText) findViewById(R.id.editTextLocalName)).getText().toString().equals("")) {
+                    Toast.makeText(this, "Debes escribir un nombre de local", Toast.LENGTH_LONG).show();
+                } else {
+                    local.setNombre(((EditText) findViewById(R.id.editTextLocalName)).getText().toString());
+                    local.setDireccion(((EditText) findViewById(R.id.editTextLocalAddress)).getText().toString());
+                    local.setDescripcion(((EditText) findViewById(R.id.editTextDecription)).getText().toString());
+                    local.setTrabajoHabitual(((Spinner) findViewById(R.id.spinnerhabitualService)).getSelectedItem().toString());
+                    if (MainActivity.getLocalRepository().addLocal(local)) {
+                        setResult(RESULT_OK);
+                        finish();
+                    } else {
+                        Toast.makeText(this, "El nombre de local a insertar ya existe", Toast.LENGTH_LONG).show();
+                    }
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
